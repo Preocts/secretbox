1c9b7bf20edfe16446d8e7c341eaac421af870e1
Preocts <preocts@gmail.com>
2021-10-09 13:42:41 -0400

    Complete implementation of new loader strategy - v2.0rc
    **This is a breaking change**
    
    This commit removes all directly implemented loader calls in favor of
    the run-time selected loader interface.
    
    - Loaders are encapsulated modules
    - Adding/Removing loaders does not impact `SecretBox` API
    - Autoload behaves as simply as before, loading environ and `.env`
    - Keyword args provided at class init will be passed to loaders
    - `load_from()` allows user controlled load order and override of
    keywords
    - An eggcellent feature

--- src/secretbox/secretbox.py
+++ src/secretbox/secretbox.py
@@ -9,7 +9,7 @@ import os
 from typing import Any
 from typing import Dict
 from typing import List
-from typing import Optional
+from typing import Literal
 from typing import Type
 
 from secretbox.awssecret_loader import AWSSecretLoader
@@ -31,51 +31,38 @@ class SecretBox:
 
     def __init__(
         self,
-        filename: str = ".env",
-        aws_sstore_name: Optional[str] = None,
-        aws_region_name: Optional[str] = None,
         auto_load: bool = False,
         debug_flag: bool = False,
+        **kwargs: Any,
     ) -> None:
         """
-        Creates an unloaded instance of class
-
-        Order of loading is environment -> .env file -> aws secrets
+        Initialize SecretBox
 
         Args:
-            filename : You can specify a `.env` formatted file and location,
-                overriding the default behavior to load the `.env` from the
-                working directory
-            aws_sstore_name : When provided, an attempt to load values from
-                named AWS secrets manager will be made. Can be provided with
-                the `AWS_SSTORE_NAME` environment variable.
-                Requires `boto3` and `boto3-stubs[secretsmanager]` to be installed.
-            aws_region_name : When provided, an attempt to load values from the given
-                AWS secrets manager found in this region will be made. Can be provided
-                with the `AWS_REGION_NAME` environment variable.
-                Requires `boto3` and `boto3-stubs[secretsmanager]` to be installed
-            auto_load : If true, the `load()` method will be auto-executed
+            auto_load : If true, environment vars and `.env` will be loaded
             load_debug : When true, internal logger level is set to DEBUG
 
+        Keywords:
+            These will be passed to all loaders when called
         """
         self.logger.setLevel(level="DEBUG" if debug_flag else "ERROR")
         self.logger.debug("Debug flag passed.")
-        env_region = os.getenv("AWS_REGION_NAME")
-        env_sstore = os.getenv("AWS_SSTORE_NAME")
-        self.filename: str = filename
-        self.aws_region = env_region if aws_region_name is None else aws_region_name
-        self.aws_sstore = env_sstore if aws_sstore_name is None else aws_sstore_name
 
         self.loaded_values: Dict[str, str] = {}
+        self.kwarg_defaults = kwargs
 
         if auto_load:
-            self.load()
+            self.load_from(["environ", "envfile"])
 
     def get(self, key: str, default: str = "") -> str:
         """Get a value by key, will return default if not found"""
         return self.loaded_values[key] if key in self.loaded_values else default
 
-    def load_from(self, loaders: List[str], **kwargs: Any) -> None:
+    def load_from(
+        self,
+        loaders: List[Literal["environ", "envfile", "awssecrets"]],
+        **kwargs: Any,
+    ) -> None:
         """
         Runs load_values from each of the listed loadered in the order they appear
 
@@ -89,7 +76,7 @@ class SecretBox:
                 Loads secrets from an AWS secret manager. Requires `aws_sstore_name`
                 and `aws_region_name` keywords to be provided or for those values
                 to be in the environment variables under `AWS_SSTORE_NAME` and
-                `AWS_REGION_NAME`. `aws_sstore` is the name of the store, not the arn.
+                `AWS_REGION_NAME`. `aws_sstore_name` is not the arn.
         """
         for loader_name in loaders:
             self.logger.debug("Loading from interface: `%s`", loader_name)
@@ -98,40 +85,23 @@ class SecretBox:
                 self.logger.warning("Loader `%s` unknown, skipping", loader_name)
                 continue
             loader = interface()
-            loader.load_values(**kwargs)
+            loader.load_values(**self._join_kwarg_defaults(kwargs))
             self.logger.debug("Loaded %d values.", len(loader.loaded_values))
-            self.loaded_values.update(loader.loaded_values)
-            self.push_to_environment()
+            self._update_loaded_values(loader.loaded_values)
+        self._push_to_environment()
 
-    # TODO(preocts): Left to preserve v1 API. Remove in v2
-    def load(self) -> None:
-        """Runs all available loaders"""
-        self.load_env_vars()
-        self.load_env_file()
-        self.load_aws_store()
-        self.push_to_environment()
+    def _update_loaded_values(self, new_values: Dict[str, str]) -> None:
+        """Update/Create instance state of loaded values with new values"""
+        self.loaded_values.update(new_values)
 
-    # TODO(preocts): Left to preserve v1 API. Remove in v2
-    def load_env_vars(self) -> None:
-        """Loads all existing `os.environ` values into state"""
-        self.load_from(["environ"])
-
-    # TODO(preocts): Left to preserve v1 API. Remove in v2
-    def load_env_file(self) -> None:
-        """Loads `.env` file or file provided"""
-        self.load_from(["envfile"], filename=self.filename)
-
-    # TODO(preocts): Left to preserve v1 API. Remove in v2
-    def load_aws_store(self) -> None:
-        """Loads secrets from AWS secret manager"""
-        self.load_from(
-            ["awssecret"],
-            aws_sstore=self.aws_sstore,
-            aws_region=self.aws_region,
-        )
-
-    def push_to_environment(self) -> None:
+    def _push_to_environment(self) -> None:
         """Pushes loaded values to local environment vars, will overwrite existing"""
         for key, value in self.loaded_values.items():
             self.logger.debug("Push, %s : ***%s", key, value[-(len(value) // 4) :])
             os.environ[key] = value
+
+    def _join_kwarg_defaults(self, new_kwargs: Dict[str, str]) -> Dict[str, str]:
+        """Update default kwargs with specific while not mutating either"""
+        base = self.kwarg_defaults.copy()
+        base.update(new_kwargs)
+        return base
--- tests/secretbox_test.py
+++ tests/secretbox_test.py
@@ -17,12 +17,39 @@ def test_load_from_with_unknown(secretbox: SecretBox, mock_env_file: str) -> Non
 
 def test_load_order_file_over_environ(secretbox: SecretBox, mock_env_file: str) -> None:
     """Loaded file should override existing environ values"""
-    secretbox.filename = mock_env_file
     altered_expected = {key: f"{value} ALT" for key, value in ENV_FILE_EXPECTED.items()}
     with patch.dict(os.environ, altered_expected):
-        secretbox.load()
+        secretbox.load_from(["environ", "envfile"], filename=mock_env_file)
         for key, value in ENV_FILE_EXPECTED.items():
             assert secretbox.get(key) == value, f"Expected: {key}, {value}"
+
+
+def test_load_order_environ_over_file(secretbox: SecretBox, mock_env_file: str) -> None:
+    """Loaded environ should override file values"""
+    altered_expected = {key: f"{value} ALT" for key, value in ENV_FILE_EXPECTED.items()}
+    with patch.dict(os.environ, altered_expected):
+        secretbox.load_from(["envfile", "environ"], filename=mock_env_file)
+        for key, value in ENV_FILE_EXPECTED.items():
+            assert secretbox.get(key) == f"{value} ALT", f"Expected: {key}, {value} ALT"
+
+
+def test_update_loaded_values(secretbox: SecretBox) -> None:
+    """Ensure we are updating state correctly"""
+    secretbox._update_loaded_values({"TEST": "TEST01"})
+    assert secretbox.get("TEST") == "TEST01"
+
+    secretbox._update_loaded_values({"TEST": "TEST02"})
+    assert secretbox.get("TEST") == "TEST02"
+
+
+def test_join_kwarg_defaults(secretbox: SecretBox) -> None:
+    """Mutables are fun, this should never create side-effects"""
+    secretbox.kwarg_defaults = {"TEST": "TEST01"}
+    new_kwargs = {"TEST": "TEST02"}
+    final_kwargs = secretbox._join_kwarg_defaults(new_kwargs)
+    assert secretbox.kwarg_defaults == {"TEST": "TEST01"}
+    assert new_kwargs == {"TEST": "TEST02"}
+    assert final_kwargs == new_kwargs
 
 
 def test_autoload_tempfile(mock_env_file: str) -> None:
