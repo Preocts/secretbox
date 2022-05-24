519086efecc45a1e2cfa926c4a9e9d9243ac65ea
Preocts <preocts@gmail.com>
2022-05-23 22:32:55 -0400

    Add loaders for ease of importing

--- src/secretbox/__init__.py
+++ src/secretbox/__init__.py
@@ -1,3 +1,13 @@
+from .awsparameterstore_loader import AWSParameterStoreLoader
+from .awssecret_loader import AWSSecretLoader
+from .envfile_loader import EnvFileLoader
+from .environ_loader import EnvironLoader
 from .secretbox import SecretBox
 
-__all__ = ["SecretBox"]
+__all__ = [
+    "AWSParameterStoreLoader",
+    "AWSSecretLoader",
+    "EnvFileLoader",
+    "EnvironLoader",
+    "SecretBox",
+]
