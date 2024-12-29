6842fd37c3225534c904c8e3b34e758ddbe9c0bb
Preocts <preocts@gmail.com>
2024-12-29 12:52:56 -0500

    Remove auto-load of environment variables (#161)
    After consideration of #160, the loading of environment variables should
    be handled exclusively in a loader. This is kicking the can on how to
    handle `os.environ` behavior when in a Windows OS. However, it makes
    sense that the SecretBox instance should be case sensitive with keys. It
    is patterned to behave like a dictionary. Changing the behavior of
    `__setitem__` and `__getitem__` here would be more surprising to a dev
    than not.

--- src/secretbox/secretbox.py
+++ src/secretbox/secretbox.py
@@ -1,31 +1,12 @@
 from __future__ import annotations
 
-import logging
-import os
-
 
 class SecretBox:
-    """
-    A key-value store optionally loaded from the local environment and other sources.
+    """A key-value store optionally loaded from the local environment and other sources."""
 
-    All keys will be normalized to upper-case. Key lookups are case-sensitive.
-    """
-
-    logger = logging.getLogger(__name__)
-
-    def __init__(self, *, load_environ: bool = False) -> None:
-        """
-        Initialize SecretBox
-
-        Keyword Args:
-            load_environ : Load existing environment variables when True.
-        """
+    def __init__(self) -> None:
+        """Create an empty SecretBox."""
         self._loaded_values: dict[str, str] = {}
-
-        if load_environ:
-            self._loaded_values = {
-                key.upper(): value for key, value in os.environ.items()
-            }
 
     @property
     def loaded_values(self) -> dict[str, str]:
@@ -50,8 +31,6 @@ class SecretBox:
         """
         Set a value assigned to a key. The key and value must be a string.
 
-        NOTE: Keys will be normalized to upper-case.
-
         Args:
             key: Key index of the value
             value: Value stored at the provided key index
@@ -66,4 +45,4 @@ class SecretBox:
             msg = f"Keys and values of a Secretbox object must be of type str. You provided: {type(key).__name__}:{type(value).__name__}"
             raise TypeError(msg)
 
-        self._loaded_values[key.upper()] = value
+        self._loaded_values[key] = value
