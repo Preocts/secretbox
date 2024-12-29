a89807208f63f4067190753a0a887e505d69ee9b
Preocts <preocts@preocts.com>
2024-12-28 23:43:49 -0500

    Remove support for 3.8

--- noxfile.py
+++ noxfile.py
@@ -33,7 +33,7 @@ nox.options.sessions = [
 
 
 @nox.session(
-    python=["3.8", "3.9", "3.10", "3.11", "3.12"],
+    python=["3.9", "3.10", "3.11", "3.12", "3.13"],
 )
 def tests_with_coverage(session: nox.Session) -> None:
     """Run unit tests with coverage saved to partial file."""
